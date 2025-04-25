46f981dc427aa55cb53c5e03ee85db8d52b038e6
nlarrea <larrea.naia@gmail.com>
2025-04-25 21:39:17 +0200

    chore: update tsconfig.json to include baseUrl and paths for module resolution, and configure JSX settings for React

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en'],
+		routing: {
+			prefixDefaultLocale: false,
+			fallbackType: 'rewrite',
+		},
+		fallback: {
+			en: 'es',
+		},
+	},
+	integrations: [react({ include: '**/react/*' })],
+});
